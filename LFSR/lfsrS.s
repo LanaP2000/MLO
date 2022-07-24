a312f1b25c66dcb0fd42fdff8ec33f06606d4c5d
Lana P <108411963+LanaP2000@users.noreply.github.com>
2022-07-25 01:58:58 +0400

    Update lfsrS.s

--- LFSR/lfsrS.s
+++ LFSR/lfsrS.s
@@ -3,35 +3,35 @@ _lfsr_assembly:
 #setup
 pushl %ebp
 movl %esp, %ebp
-movl 8(%ebp), %eax            #seed
+movl 8(%ebp), %eax        #seed
 
 #body
 .L1:
-cmpl $0,%eax            #seed == 0
+cmpl $0,%eax              #seed == 0
 jne .L2
-incl %eax            #seed++
+incl %eax                 #seed++
 jmp .END
 
 .L2:
-movl %eax, %ebx            #seed
-shrl $2, %ebx            #>>2
+movl %eax, %ebx           #seed
+shrl $2, %ebx             #>>2
 
-movl %eax, %ecx            #seed
-shrl $6, %ecx            #>>6
+movl %eax, %ecx           #seed
+shrl $6, %ecx             #>>6
 
-xorl %ecx, %ebx            #^ bitwise
-xorl %eax, %ebx            #^ bitwise
+xorl %ecx, %ebx           #^ bitwise
+xorl %eax, %ebx           #^ bitwise
 
 movl %eax, %ecx
-shrl $7, %ecx            #>>7
+shrl $7, %ecx             #>>7
 
-xorl %ecx, %ebx            #^ bitwise
+xorl %ecx, %ebx           #^ bitwise
 andl $1, %ebx
 andl $1, %ebx
-movl %ebx, %edx            #bits = ............
+movl %ebx, %edx           #bits = ............
 je .L3
 
-shrl $1, %eax            #>>1
+shrl $1, %eax             #>>1
 
 sall $31, %ebx            #<<31
 orl %ebx, %eax            #| bitwise
@@ -41,7 +41,7 @@ jmp .END
 cmpl $0, %eax
 
 
-shrl $1, %eax            #>>1
+shrl $1, %eax             #>>1
 jmp .END
 
 #end
